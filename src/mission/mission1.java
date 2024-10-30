e347c22be9c185d14b7a3325a42108a941ac3f49
tdronnie <senyvee9@gmail.com>
2024-10-30 19:24:45 +0900

    feat: mission1 추가

---
+++ src/mission/mission1.java
@@ -0,0 +1,10 @@
+package mission;
+
+public class mission1 {
+
+    public static void main(String[] args) {
+        System.out.println("안녕하세요!");
+        System.out.println("저는 이세은입니다.");
+        System.out.println("스터디 완주까지 다들 화이팅!!");
+    }
+}
