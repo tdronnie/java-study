8aa9126008f0ba61ad706b56819c49945155f5bf
tdronnie <senyvee9@gmail.com>
2024-10-31 14:45:23 +0900

    feat: mission2 추가

---
+++ src/mission/mission2.java
@@ -0,0 +1,36 @@
+package mission;
+
+public class mission2 {
+
+    public static class ItemCounter {
+        private static int totalCount;
+
+        static void addItem(){
+            totalCount++;
+        }
+
+        public int getTotalCount(){
+            return this.totalCount;
+        }
+    }
+
+    public static void main(String[] args) {
+        ItemCounter counter1 = new ItemCounter();
+        ItemCounter counter2 = new ItemCounter();
+
+        System.out.println(ItemCounter.totalCount); //ItemCounter의 static 변수 totalCount 확인
+
+        counter1.addItem(); //counter1 totalCount 증가
+        counter1.addItem(); //counter1 totalCount 증가
+        System.out.println(counter1.getTotalCount()); // counter1 인스턴스의 TotalCount 확인
+
+        System.out.println(counter2.getTotalCount()); //counter1 변수 처리 후 counter2 인스턴스의 TotalCount 확인
+        counter2.addItem(); //counter2 totalCount 증가
+        System.out.println(counter2.getTotalCount()); //counter2 변수 처리 후 counter2 인스턴스의 TotalCount 확인
+
+
+    }
+
+}
+
+
